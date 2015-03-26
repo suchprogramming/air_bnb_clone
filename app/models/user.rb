82e85720b452307da7b4349e23a67d395ec5347e
Tyler <whitsett.tyler@gmail.com>
2015-03-26 14:51:31 -0700

    set up posts relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :posts, dependent: :destroy
+  validates :username, :presence => true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
