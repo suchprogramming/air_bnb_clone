1a995cdd0d1eb3cf2dd698c8461cbdb978554011
Tyler <whitsett.tyler@gmail.com>
2015-03-26 16:46:09 -0700

    add post for nesting with users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,18 @@
 FactoryGirl.define do
+
   factory :user do
     email('user@email.com')
     username('doge')
     password('12345678')
     password_confirmation('12345678')
+  end
+
+  factory :post do
+    title('Post Title')
+    post_text('weeee this is the post text')
+    city('Portland')
+    state('Oregon')
+    zip_code('97224')
   end
 
 end
