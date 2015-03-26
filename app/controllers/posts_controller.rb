ee67bd6799d70c661b0ab79376a0d77430c137ba
Tyler <whitsett.tyler@gmail.com>
2015-03-26 14:48:06 -0700

    add factory for user creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,57 @@
 class PostsController < ApplicationController
 
+  before_filter :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:success] = "Post Added!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = 'Post Successfully Updated!'
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if current_user.id == @post.user_id
+      @post.destroy
+      flash[:success] = 'Post Successfully Deleted!'
+      redirect_to posts_path
+    else
+      flash[:danger] = "Not So Fast!"
+      redirect_to posts_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :post_text)
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    email('user@email.com')
+    username('doge')
+    password('12345678')
+    password_confirmation('12345678')
   end
 
 end
